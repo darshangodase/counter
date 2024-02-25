7289c24cebcf5372a5643bb725a6599edaa39cef
darshangodase <darshangodase10@gmail.com>
2024-02-25 18:16:34 +0530

    second commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const button1 = document.querySelector('#bt1');
 const button2 = document.querySelector('#bt2');
 
  let i=0;
+ display.textContent=i;
 button1.addEventListener('click', () => {
     display.textContent= i--;
     if(i<0)
