b4d703041b632a5e3cbff134c436fe11663887fd
darshangodase <darshangodase10@gmail.com>
2024-02-25 18:09:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const display = document.querySelector('.display');
+const button1 = document.querySelector('#bt1');
+const button2 = document.querySelector('#bt2');
+
+ let i=0;
+button1.addEventListener('click', () => {
+    display.textContent= i--;
+    if(i<0)
+    {
+        display.style.color='red';
+    }
+    else if(i==0)
+    {
+        display.style.color='black';
+    }
+    else
+    {
+        display.style.color='green';
+    }
+});
+button2.addEventListener('click', () => {
+    display.textContent= i++;
+    if(i<0)
+    {
+        display.style.color='red';
+    }
+    else if(i==0)
+    {
+        display.style.color='black';
+    }
+    else
+    {
+        display.style.color='green';
+    }
+});
